833fe05b1700caf6eb121e05fc0da338684f7186
suil0304 <eagle030410@gmail.com>
2026-03-25 22:57:51 +0900

    yeah it's awesome dude

--- 6/1157/1157.c
+++ 6/1157/1157.c
@@ -1,35 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-
-// 컴파일 에러는 뭐야
-
-/*
-
-------------------------------------------------------
-오류
-에러 유형: 제출 코드 컴파일 에러(TargetCompilationFailed)
-메세지:
-Main.c: In function ‘main’:
-Main.c:11:13: warning: implicit declaration of function ‘strupr’; did you mean ‘strstr’? [-Wimplicit-function-declaration]
-   11 |     upper = strupr(str);
-      |             ^~~~~~
-      |             strstr
-Main.c:11:11: warning: assignment to ‘char *’ from ‘int’ makes pointer from integer without a cast [-Wint-conversion]
-   11 |     upper = strupr(str);
-      |           ^
-Main.c:8:5: warning: ignoring return value of ‘scanf’ declared with attribute ‘warn_unused_result’ [-Wunused-result]
-    8 |     scanf("%s", str);
-      |     ^~~~~~~~~~~~~~~~
-/usr/bin/ld: /tmp/ccnUHElk.o: in function `main':
-Main.c:(.text.startup+0x61): undefined reference to `strupr'
-collect2: error: ld returned 1 exit status
-------------------------------------------------------
-
-일단 VScode에서는 정상 작동함
-아마도
-음
-
-*/
+#include <ctype.h>
 
 int main() {
     int asciiCounts[26] = {0, };
@@ -37,31 +8,31 @@ int main() {
     char str[1000001];
     scanf("%s", str);
 
-    char* upper;
-    upper = strupr(str);
-
     int length;
-    length = strlen(upper);
+    length = strlen(str);
 
     for(int i = 0; i < length; i++) {
-        char character = upper[i];
-        asciiCounts[((int) character) - 65]++;
+        char character = str[i];
+        char uppered = (char) toupper((int) character);
+        asciiCounts[((int) uppered) - 65]++;
+
+        str[i] = uppered;
     }
 
     int maxIndex = 0;
+    int maxCount = -1;
+    char result = '?';
     for(int i = 0; i < 26; i++) {
-        if(asciiCounts[maxIndex] < asciiCounts[i]) {
+        if(maxCount < asciiCounts[i]) {
+            maxCount = asciiCounts[i];
             maxIndex = i;
+            result = (char) (maxIndex + 65);
+        }        
+        else if(maxCount == asciiCounts[i]) {
+            result = '?';
         }
     }
-
-    for(int i = 0; i < 26; i++) {
-        if(asciiCounts[maxIndex] == asciiCounts[i] && maxIndex != i) {
-            printf("?");
-            return 0;
-        }
-    }
-    printf("%c", (char) (65 + maxIndex));
+    printf("%c", result);
 
     return 0;
 }
